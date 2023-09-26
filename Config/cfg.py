1283487195afeb0a4525de5050b568ffe42413cc
Shizamuru <57797277+shizamuru-dev@users.noreply.github.com>
2023-09-26 21:34:56 +0300

    Update cfg.py
    add comments

--- Config/cfg.py
+++ Config/cfg.py
@@ -1,6 +1,6 @@
 settings = {
-    "token": "",
-    "name": "",
-    "id": 1,
-    "prefix": "!"
+    "token": "" #Bot token from the discord dev platform,
+    "name": "" #Bot name,
+    "id": 1 # Application id from the discord dev platform,
+    "prefix": "!" # Chat command prefix ( I advise you to use ! or $ )
 }
